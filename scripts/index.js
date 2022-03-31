a29fcebcb0ff57b5513f99d75d3472c4b8eed250
Pavel Sitnikov <sitnikovpa@gmail.com>
2022-03-31 23:23:35 +0500

    feat: добавить интерактив

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,34 @@
+// popup open/close
+let editing = document.querySelector(".profile__edit-button");
+let modalWindow = document.querySelector(".popup");
+let modalCloseBtn = modalWindow.querySelector(".popup__close");
 
+function toggleModalWindow() {
+  modalWindow.classList.toggle("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+}
+
+editing.addEventListener("click", toggleModalWindow);
+
+modalCloseBtn.addEventListener("click", toggleModalWindow);
+
+// popup profile changes
+
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__name");
+let jobInput = formElement.querySelector(".popup__description");
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let sendBtn = document.querySelector(".popup__send");
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  toggleModalWindow();
+}
+
+sendBtn.addEventListener("click", formSubmitHandler);
